d295fa27420308eec0439714bf5f99bded794b4d
Mubarak Olajuwon <olajuwonmubarak@hotmail.com>
2022-07-05 19:59:27 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *print _putchar twice 
+ * *
+ * */
+void print_alphabet(void)
+	{
+		char j;
+
+		for (j= 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+
+		}
+		_putchar(j);
+	}
+
