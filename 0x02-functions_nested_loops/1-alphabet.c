5fe2a7341f5926bca02c0ce1a7498481df1d5ad4
Olajuwon Mubarak <olajuwonmubarak@hotmail.com>
2022-07-05 21:01:50 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * *print _putchar twice 
- * *
- * */
+ * print _putchar twice 
+ * return: 0 Success
+ */
 void print_alphabet(void)
+{
+	char j;
+
+	for (j= 'a'; j <= 'z'; j++)
 	{
-		char j;
-
-		for (j= 'a'; j <= 'z'; j++)
-		{
-			_putchar(j);
-
-		}
 		_putchar(j);
+
 	}
+	_putchar(j);
+}
 
