232b96633e0ba278587aa48889b9e25937add10d
Olajuwon Mubarak <olajuwonmubarak@hotmail.com>
2022-07-07 20:37:01 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- ** print_most_numbers - prints the numbers, from 0 to 9, followed by a new line
- ** Return: 0
+ * print_most_numbers - prints the numbers, from 0 to 9, followed by a new line
+ * Return: 0
  */
 void print_most_numbers(void)
 {
@@ -12,8 +12,8 @@ void print_most_numbers(void)
 	{
 		if (n != 2 && n != 4)
 		{
-																		_putchar('0' + n);
-																     }
+			_putchar('0' + n);
+		}
 		n++;
 	}
 	_putchar('\n');
