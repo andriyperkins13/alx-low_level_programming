fad116440f3dfee63c95cf2ac3258a07b25587d5
Olajuwon Mubarak <olajuwonmubarak@hotmail.com>
2022-07-22 20:27:51 +0100

    D

--- 0x0A-agrc_agrv/0-whatsmyname.c
+++ 0x0A-agrc_agrv/0-whatsmyname.c
@@ -1,15 +1,13 @@
-
 #include <stdio.h>
 
 /**
- * main - prints the name of the program
- * @argc: number of arguments passed to the function
- * @argv: argument vector of pointers to strings
- *
- * Return: always 0
- */
+* main -> this is a function to print its name
+* @argc: argc parameter
+* @argv: an array of a command listed
+* Return: 0 for success
+*/
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
 	return (0);
 }
