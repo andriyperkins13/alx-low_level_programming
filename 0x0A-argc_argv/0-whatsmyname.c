dd6ca99ae2927733cef1de649820d9f93ca8244c
Olajuwon Mubarak <olajuwonmubarak@hotmail.com>
2022-07-23 03:32:16 +0100

    Name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,33 +1,13 @@
-#include "main.h"
-
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - a program that prints it name.
- *
- *   * @argc: argument count
- *
- *    * @argv: argument value (a strings that comes after calling a function)
- *
- *     * Return: always 0
- *
- *      */
-
-
-
-int main(int argc, char *argv[])
-
+ * main - function
+ * @argc: offset count
+ * @argv: offset value
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
 {
-
-		if (argc > 0)
-
-					printf("%s\n", argv[0]);
-
-
-
-			return (0);
-
+	printf("%s\n", *argv);
+	return (0);
 }
