cb1c69fbf485f99b4b5f3056661e8a81988d936d
Olajuwon Mubarak <olajuwonmubarak@hotmail.com>
2022-10-03 20:58:26 +0100

    Update 1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -fPIC *.c -shared -o liball.so
+gcc -Wall -Wextra -Werror -pedantic *.c -c -fPIC
+gcc -Wall -Wextra -Werror -pedantic *.o -shared -o liball.so
+export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
